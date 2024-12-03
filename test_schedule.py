f33c05de8cb496a29678a54cd761db88b1c9b203
Dmitry Meltsov <m.dima3044@gmail.com>
2024-12-04 01:17:53 +0500

    test

--- test_schedule.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-
-
-print(datetime.now().strftime('%H:%M %d %m %Y' ))
-
-correction = timedelta(days = 2)
-
-print((datetime.now()+correction).strftime('%H:%M %d %m %Y' ))
